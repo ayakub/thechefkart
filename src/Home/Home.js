940ced0f7f48418b5816be5db1f3375466f1c025
ayakub <ayakub1086@gmail.com>
2022-12-11 03:52:51 +0600

    added sorting

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,15 +1,72 @@
 import React, { useEffect, useState } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+
 
 const Home = () => {
-    const [list, setList] = useState('')
+    const [lists, setLists] = useState(null);
     useEffect(() => {
         fetch('list.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setLists(data))
     }, [])
+    console.log(lists);
+
+
+
     return (
         <div>
 
+            <Table className="table table-compact w-full">
+                <Thead className=" text-orange-600">
+                    <Tr>
+                        <Th>Id</Th>
+                        <Th>first_name</Th>
+                        <Th>last_name
+
+                        </Th>
+                        <Th>email</Th>
+                        <Th>gender</Th>
+                        <Th>ip_address</Th>
+                        <Th>airport code</Th>
+                        <Th>time</Th>
+                        <Th>status</Th>
+                        <Th>mobile</Th>
+                        <Th>area</Th>
+                        <Th>show</Th>
+                        <Th>edit</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {/* data map */}
+                    {
+                        lists?.map((list, i) => <Tr>
+                            <Th>{i + 1}</Th>
+                            <Td>{list.first_name}</Td>
+                            <Td>{list.last_name}</Td>
+                            <Td>{list.email}</Td>
+                            <Td>{list.gender}</Td>
+                            <Td>{list.ip_address}</Td>
+                            <Td>{list.airport_code}</Td>
+                            <Td>{list.time}</Td>
+                            <Td style={{
+                                backgroundColor: list.status ? 'green' : 'red',
+                            }}
+                            ><span className='text-white'>{list.status === true ? 'true' : 'false'}</span></Td>
+                            <Td>{list.mobile}</Td>
+                            <Td>{list.area}</Td>
+                            <Td>{list.show}</Td>
+                            <Td>{list.edit}</Td>
+                        </Tr>)
+                    }
+
+
+
+
+
+
+                </Tbody>
+            </Table>
         </div>
     );
 };
